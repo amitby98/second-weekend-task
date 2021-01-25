47c420baa77cc1ab93e08c0bf6faa3794eff24b6
amitby98 <77382577+amitby98@users.noreply.github.com>
2021-01-25 19:50:42 +0200

    finished the code
    i changed the code and started to work on css

--- table.js
+++ table.js
@@ -1,137 +1,197 @@
-let arr = [
-  {
-    startedAt: new Date("2021-01-20:15:00"),
-    finishedAt: new Date("2021-01-20:20:00"),
-    taskGiven: 24,
-    taskFinished: 20,
-    Topic: "HTML",
-  },
-  {
-    startedAt: new Date("2021-01-20:16:00"),
-    finishedAt: new Date("2021-01-20:20:00"),
-    taskGiven: 28,
-    taskFinished: 18,
-    Topic: "CSS",
-  },
-  {
-    startedAt: new Date("2021-01-20:11:00"),
-    finishedAt: new Date("2021-01-20:12:00"),
-    taskGiven: 25,
-    taskFinished: 20,
-    Topic: "GitHub",
-  },
-  {
-    startedAt: new Date("2021-01-20:09:00"),
-    finishedAt: new Date("2021-01-20:16:00"),
-    taskGiven: 10,
-    taskFinished: 3,
-    Topic: "More HTML",
-  },
-  {
-    startedAt: new Date("2021-01-20:14:00"),
-    finishedAt: new Date("2021-01-20:16:30"),
-    taskGiven: 25,
-    taskFinished: 25,
-    Topic: "JavaScript",
-  },
-  {
-    startedAt: new Date("2021-01-20:11:00"),
-    finishedAt: new Date("2021-01-20:12:30"),
-    taskGiven: 16,
-    taskFinished: 8,
-    Topic: "Loops",
-  },
-  {
-    startedAt: new Date("2021-01-20:09:00"),
-    finishedAt: new Date("2021-01-20:15:30"),
-    taskGiven: 24,
-    taskFinished: 0,
-    Topic: "Conditions",
-  },
-  {
-    startedAt: new Date("2021-01-20:10:00"),
-    finishedAt: new Date("2021-01-20:12:00"),
-    taskGiven: 30,
-    taskFinished: 20,
-    Topic: "Arrays",
-  },
-  {
-    startedAt: new Date("2021-01-20:14:30"),
-    finishedAt: new Date("2021-01-20:18:00"),
-    taskGiven: 20,
-    taskFinished: 15,
-    Topic: "Object",
-  },
+"use strict";
+let task1 = {
+  startedAt: new Date("2021-01-20:15:00"),
+  finishedAt: new Date("2021-01-20:20:00"),
+  totalTime: 0,
+  tasksGiven: 24,
+  tasksFinished: 20,
+  tasksFinishedPercent: 0,
+  topic: "HTML",
+};
+
+let task2 = {
+  startedAt: new Date("2021-01-20:16:00"),
+  finishedAt: new Date("2021-01-20:20:00"),
+  totalTime: 0,
+  tasksGiven: 28,
+  tasksFinished: 18,
+  tasksFinishedPercent: 0,
+  topic: "CSS",
+};
+
+let task3 = {
+  startedAt: new Date("2021-01-20:11:00"),
+  finishedAt: new Date("2021-01-20:17:00"),
+  totalTime: 0,
+  tasksGiven: 25,
+  tasksFinished: 20,
+  tasksFinishedPercent: 0,
+  topic: "GitHub",
+};
+
+let task4 = {
+  startedAt: new Date("2021-01-20:12:00"),
+  finishedAt: new Date("2021-01-20:16:00"),
+  totalTime: 0,
+  tasksGiven: 10,
+  tasksFinished: 3,
+  tasksFinishedPercent: 0,
+  topic: "More HTML",
+};
+
+let task5 = {
+  startedAt: new Date("2021-01-20:14:00"),
+  finishedAt: new Date("2021-01-20:16:30"),
+  totalTime: 0,
+  tasksGiven: 25,
+  tasksFinished: 25,
+  tasksFinishedPercent: 0,
+  topic: "JavaScript",
+};
+
+let task6 = {
+  startedAt: new Date("2021-01-20:11:00"),
+  finishedAt: new Date("2021-01-20:12:30"),
+  totalTime: 0,
+  tasksGiven: 16,
+  tasksFinished: 8,
+  tasksFinishedPercent: 0,
+  topic: "Loops",
+};
+
+let task7 = {
+  startedAt: new Date("2021-01-20:12:00"),
+  finishedAt: new Date("2021-01-20:15:30"),
+  totalTime: 0,
+  tasksGiven: 24,
+  tasksFinished: 0,
+  tasksFinishedPercent: 0,
+  topic: "Conditions",
+};
+
+let task8 = {
+  startedAt: new Date("2021-01-20:10:00"),
+  finishedAt: new Date("2021-01-20:12:00"),
+  totalTime: 0,
+  tasksGiven: 30,
+  tasksFinished: 20,
+  tasksFinishedPercent: 0,
+  topic: "Arrays",
+};
+
+let task9 = {
+  startedAt: new Date("2021-01-20:14:30"),
+  finishedAt: new Date("2021-01-20:18:00"),
+  totalTime: 0,
+  tasksGiven: 20,
+  tasksFinished: 15,
+  tasksFinishedPercent: 0,
+  topic: "Object",
+};
+
+let taskTable = [task1, task2, task3, task4, task5, task6, task7, task8, task9];
+
+for (let task of taskTable) {
+  task["totalTime"] = (task.finishedAt - task.startedAt) / 3600000;
+  task["tasksFinishedPercent"] =
+    Math.floor((task.tasksFinished / task.tasksGiven) * 100 + 0.5) + " %";
+  task.startedAt = task.startedAt.toLocaleTimeString().replace(":00", "");
+  task.finishedAt = task.finishedAt.toLocaleTimeString().replace(":00", "");
+}
+
+let tableHead = [
+  "Started At",
+  "Finished At",
+  "Total Time Spent",
+  "Tasks Given",
+  "Tasks Finished",
+  "Tasks Finished %",
+  "Topic",
 ];
 
-function tableCreate() {
-  let table = document.createElement("table");
-  for (let i = 0; i < arr.length + 1; i++) {
-    let row = table.insertRow(i);
-    let cell1 = row.insertCell(0);
-    let cell2 = row.insertCell(1);
-    let cell3 = row.insertCell(2);
-    let cell4 = row.insertCell(3);
-    let cell5 = row.insertCell(4);
-    let cell6 = row.insertCell(5);
-    let cell7 = row.insertCell(6);
-    if (i === 0) {
-      cell1.innerHTML = "Started At";
-      cell2.innerHTML = "Finished At";
-      cell3.innerHTML = "Total Time Spent";
-      cell4.innerHTML = "Task Given";
-      cell5.innerHTML = "Task Finished";
-      cell6.innerHTML = "Task Finished %";
-      cell7.innerHTML = "Topic";
-      for (let j = 0; j < 7; j++)
-        row.childNodes[j].style.backgroundColor = "MediumPurple";
-    } else {
-      let start =
-        (arr[i - 1]["startedAt"].getHours() < 10 ? "0" : "") +
-        arr[i - 1]["startedAt"].getHours() +
-        ":" +
-        (arr[i - 1]["startedAt"].getMinutes() < 10 ? "0" : "") +
-        arr[i - 1]["startedAt"].getMinutes();
-      let end =
-        (arr[i - 1]["finishedAt"].getHours() < 10 ? "0" : "") +
-        arr[i - 1]["finishedAt"].getHours() +
-        ":" +
-        (arr[i - 1]["finishedAt"].getMinutes() < 10 ? "0" : "") +
-        arr[i - 1]["finishedAt"].getMinutes();
+let table = document.createElement("table");
+let headerow = document.createElement("tr");
 
-      let t1 = start.split(":");
-      let t2 = end.split(":");
-      let d1 = new Date(0, 0, 0, parseInt(t1[0], 10), parseInt(t1[1], 10));
-      let d2 = new Date(0, 0, 0, parseInt(t2[0], 10), parseInt(t2[1], 10));
-      let time =
-        d2.getHours() +
-        d2.getMinutes() / 60 -
-        d1.getHours() +
-        d1.getMinutes() / 60;
-
-      let percent = parseInt(
-        (arr[i - 1]["taskFinished"] * 100) / arr[i - 1]["taskGiven"],
-        10
-      );
-
-      cell1.innerHTML = start;
-      cell2.innerHTML = end;
-      cell3.innerHTML = time;
-      cell4.innerHTML = arr[i - 1]["taskGiven"];
-      cell5.innerHTML = arr[i - 1]["taskFinished"];
-      cell6.innerHTML = percent + "%";
-      cell7.innerHTML = arr[i - 1]["Topic"];
-
-      if (time < 2) row.childNodes[2].style.backgroundColor = "pink";
-      else if (time >= 2 && time <= 5)
-        row.childNodes[2].style.backgroundColor = "HotPink";
-      if (time > 5) row.childNodes[2].style.backgroundColor = "PaleVioletRed";
-
-      if (percent < 50) row.childNodes[5].style.backgroundColor = "PowderBlue";
-      else if (percent >= 50 && percent <= 75)
-        row.childNodes[5].style.backgroundColor = "SkyBlue";
-      if (percent > 75) row.childNodes[5].style.backgroundColor = "SteelBlue";
-    }
-  }
-  document.getElementsByTagName("body")[0].appendChild(table);
+for (let header in tableHead) {
+  let headerName = document.createElement("th");
+  headerName.textContent = tableHead[header];
+  headerow.appendChild(headerName);
 }
+
+table.appendChild(headerow);
+table.appendChild(tableData(taskTable[0]));
+table.appendChild(tableData(taskTable[1]));
+table.appendChild(tableData(taskTable[2]));
+table.appendChild(tableData(taskTable[3]));
+table.appendChild(tableData(taskTable[4]));
+table.appendChild(tableData(taskTable[5]));
+table.appendChild(tableData(taskTable[6]));
+table.appendChild(tableData(taskTable[7]));
+table.appendChild(tableData(taskTable[8]));
+document.body.appendChild(table);
+
+function tableData(obj) {
+  let tableRow = document.createElement("tr");
+  for (let tData in obj) {
+    let tableD = document.createElement("td");
+    tableD.textContent = obj[tData];
+    tableRow.appendChild(tableD);
+  }
+  return tableRow;
+}
+
+// document.write("<table class = 'tableS'>");
+// document.write("<tr class = 'rowS'>");
+// for (let head of tableHead) {
+//   document.write("<th class = 'header'>" + head + "</th>");
+// }
+// document.write("</tr>");
+
+// for (let task of taskTable) {
+//   document.write("<tr class = 'rowS'>");
+//   document.write("<td class = 'dataS'>" + task.startedAt + "</td>");
+//   document.write("<td class = 'dataS'>" + task.finishedAt + "</td>");
+//   if (task.totalTime <= 2) {
+//     let className = "totalTimeGood";
+//     document.write(
+//       `<td class="${className}"> ${task.totalTime + " Hours"} </td>`
+//     );
+//   }
+//   if (task.totalTime > 2 && task.totalTime <= 5) {
+//     let className = "totalTimeX";
+//     document.write(
+//       `<td class="${className}"> ${task.totalTime + " Hours"} </td>`
+//     );
+//   }
+//   if (task.totalTime > 5) {
+//     let className = "totalTimeEs";
+//     document.write(
+//       `<td class="${className}"> ${task.totalTime + " Hours"}</td>`
+//     );
+//   }
+//   document.write("<td class = 'dataS'>" + task.tasksGiven + "</td>");
+//   document.write("<td class = 'dataS'>" + task.tasksFinished + "</td>");
+
+// if (task.tasksFinishedPercent < 50) {
+//   let className = "totalPerGood";
+//   document.write(
+//     `<td class="${className}" > ${task.tasksFinishedPercent + " %"} </td>`
+//   );
+// }
+// if (task.tasksFinishedPercent >= 50 && task.tasksFinishedPercent <= 75) {
+//   let className = "totalPerX";
+//   document.write(
+//     `<td class="${className}"> ${task.tasksFinishedPercent + " %"} </td>`
+//   );
+// }
+// if (task.tasksFinishedPercent > 75) {
+//   let className = "totalPerEs";
+//   document.write(
+//     `<td class="${className}"> ${task.tasksFinishedPercent + " %"}</td>`
+//   );
+// }
+//   document.write("<td class = 'dataS'>" + task.topic + "</td>");
+
+//   document.write("</tr>");
+// }
+// document.write("</table>");
